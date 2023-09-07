24ac0ffadb296c456958c9fba8e037e92b775e77
johnson1202 <shinyjohnsonn@gmail.com>
2023-09-07 11:37:37 +0800

    fixed env, server_url

--- app/utils/global.js
+++ app/utils/global.js
@@ -1,3 +1,13 @@
+const url =
+  process.env.NODE_ENV === "development"
+    ? process.env.DEVELOPMENT_URL
+    : process.env.PRODUCTION_URL;
 
-const url = process.env.NODE_ENV === "development" ? process.env.DEVELOPMENT_URL : process.env.PRODUCTION_URL
+export const API_URL =
+  process.env.NODE_ENV == "production"
+    ? process.env.PRODUCTION_URL
+    : process.env.NODE_ENV == "staging"
+    ? process.env.STAGING_API_URL
+    : process.env.DEVELOPMENT_API_URL;
+
 global.SERVER_URL = url;
